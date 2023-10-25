c15ac61621703f5312493db58d9938f107786e58
Sebastian Hothaza <sebastianhothaza@gmail.com>
2023-10-25 16:19:24 -0400

    Added remove functionality

--- src/index.js
+++ src/index.js
@@ -35,12 +35,41 @@ const treeFactory = (arr, root) => {
         return cur;
     }
 
-    
+    function remove(value, cur = this.root){
+        if (cur==null){ //We reached leaf without finding a node to delete
+            
+            return cur;
+        }
+        if (cur.data == value){ // This is the node we want to delete
+            if (!cur.right && !cur.left){
+                return null;
+            } else if (cur.right && cur.left){
+                // We must find the the in-order successor of the node, copy it, then delete it
+                // 1. Find the in-order successor
+                let successor = cur.right; // known to be non-null
+                while (successor.left){
+                    successor = successor.left;
+                }
+                // 2. Copy contents of succesor to node, then delete the in-order successor
+                cur.data = successor.data;
+                cur.right = remove(successor.data, cur.right);
+                return cur;
+            } else{
+                return (cur.left) ?  cur.left :  cur.right;
+            }
+        }
+        if (value > cur.data){
+            cur.right = remove(value, cur.right);
+        } else {
+            cur.left = remove(value, cur.left);
+        }
+        return cur;
+    }
 
     return{
         get root(){return root},
         set root(newRoot){root=newRoot},
-        insert
+        insert, remove
     };
 }
 
@@ -59,8 +88,8 @@ const prettyPrint = (node, prefix = "", isLeft = true) => {
     }
   };
 
-  const myTree = treeFactory([1,2,3,4,5,6,7,8,9]);
-  prettyPrint(myTree.root);
-  prettyPrint(myTree.insert(10));
+const myTree = treeFactory([1,2,3,4,5,6,7,8,9]);
+prettyPrint(myTree.root);
+
 
   
