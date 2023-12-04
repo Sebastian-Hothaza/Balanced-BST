465562b7e9590686dfd1b752031d3a2aee078856
Sebastian Hothaza <sebastianhothaza@gmail.com>
2023-12-04 14:16:47 -0500

    Added traversal functionality including optional callback fn implementation.

--- src/index.js
+++ src/index.js
@@ -41,9 +41,9 @@ const treeFactory = (arr, root) => {
             return cur;
         }
         if (cur.data == value){ // This is the node we want to delete
-            if (!cur.right && !cur.left){
+            if (!cur.right && !cur.left){ //case I; node to delete is a single leaf. EZPZ
                 return null;
-            } else if (cur.right && cur.left){
+            } else if (cur.right && cur.left){ //Case III; node to delete has 2 children
                 // We must find the the in-order successor of the node, copy it, then delete it
                 // 1. Find the in-order successor
                 let successor = cur.right; // known to be non-null
@@ -54,7 +54,7 @@ const treeFactory = (arr, root) => {
                 cur.data = successor.data;
                 cur.right = remove(successor.data, cur.right);
                 return cur;
-            } else{
+            } else{ // Case II, node has 1 child
                 return (cur.left) ?  cur.left :  cur.right;
             }
         }
@@ -66,10 +66,60 @@ const treeFactory = (arr, root) => {
         return cur;
     }
 
+    function find(value, cur = this.root){
+        if (!cur) return null;
+        if (cur.data == value) return cur;
+        if (value > cur.data) return find(value, cur.right);
+        return find(value, cur.left);
+    }
+
+    // Traverse the BST in breadth-first level order optionally applying callback fn.
+    function levelOrder(callback, queue = [this.root], result=[]){
+        if (queue.length == 0) return result //we've reached end of the queue, thus visited all nodes
+
+        callback? result.push(callback(queue[0].data)) : result.push(queue[0].data) // Visit front of queue
+
+        if (queue[0].left) queue.push(queue[0].left) //If children are non-null, push them onto the queue
+        if (queue[0].right) queue.push(queue[0].right)
+
+        queue.splice(0,1); // Remove the visited node from the queue
+    
+        return levelOrder(callback, queue, result)
+    }
+
+    // Traverse the BST in depth-frist in-order optionally applying callback fn.
+    function inOrder(callback, cur=this.root, result=[]){
+        if (!cur) return;
+        inOrder(callback, cur.left, result);
+        callback? result.push(callback(cur.data)) : result.push(cur.data)
+        inOrder(callback, cur.right, result);
+        return result;
+    }
+
+    // Traverse the BST in depth-frist pre-order optionally applying callback fn.
+    function preOrder(callback, cur=this.root, result=[]){
+        if (!cur) return;
+        callback? result.push(callback(cur.data)) : result.push(cur.data)
+        preOrder(callback, cur.left, result);
+        preOrder(callback, cur.right, result);
+        return result;
+    }
+
+    // Traverse the BST in depth-frist post-order optionally applying callback fn.
+    function postOrder(callback, cur=this.root, result=[]){
+        if (!cur) return;
+        postOrder(callback, cur.left, result);
+        postOrder(callback, cur.right, result);
+        callback? result.push(callback(cur.data)) : result.push(cur.data)
+        return result;
+    }
+
+    
+
     return{
         get root(){return root},
         set root(newRoot){root=newRoot},
-        insert, remove
+        insert, remove, find, levelOrder, inOrder, preOrder, postOrder
     };
 }
 
@@ -86,10 +136,18 @@ const prettyPrint = (node, prefix = "", isLeft = true) => {
     if (node.left !== null) {
       prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
     }
-  };
-
-const myTree = treeFactory([1,2,3,4,5,6,7,8,9]);
-prettyPrint(myTree.root);
+};
 
 
-  
+function multiply10(x){
+    return x*10;
+}
+
+
+const myTree = treeFactory([10,20,30,40]);
+const myTreeB = treeFactory([1,2,3,4,5,6,7,8,9]);
+
+
+
+
+
